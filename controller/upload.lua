25229cfd1ad61d40f7720941320e7a92519fb243
Pan Zhiyong <pan.zhiyong@cybertech.com.cn>
2015-11-13 11:30:35 +0800

    refactor: remove the Query controller, rewrite URL from query/index to upload/query.

--- controller/upload.lua
+++ controller/upload.lua
@@ -49,3 +49,23 @@ function append()
         post_to(nil)
     end
 end
+
+-- Query file info.
+function query()
+    path = ngx.req.raw_header():match('^%a+ (.*) HTTP/')
+    i, _ = path:find('/group')
+    if i then
+        fileid = path:sub(i + 1)
+
+        local request = get_instance().request
+        status, info = request:query_file_info(fileid)
+        ngx.status = status
+        if info then
+            local cjson = require "cjson"
+            ngx.say(cjson.encode(info))
+        end
+        ngx.exit(ngx.HTTP_OK)
+    else
+        ngx.status = 400
+    end
+end
